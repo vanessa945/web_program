73d79200770902c1723ed7f35b0b0728535a772b
vanessa945 <110395461+vanessa945@users.noreply.github.com>
2026-03-24 21:55:34 +0800

    Add files via upload

---
+++ A1133301_王心妤_作業1/HW_2.php
@@ -0,0 +1,103 @@
+<HTML>
+<head>
+    <meta charset="utf-8" />
+    <title>2026一起fun暑假夏令營資訊</title>
+</head>
+    
+<body>
+    <body style="background-color: lightyellow;">
+    <h2><p style="text-align: center;">2026一起fun暑假</p><h2>
+    <h3><p style="text-align: center;">2026 KIDS SUMMER CAPM</p></h3>
+    <h3><p style="text-align: center;">這個夏天，讓孩子全方位成長！  給孩子一個不一樣的暑假！</p></h3>
+
+    <table border="3" style="width:100%; text-align:center; border-collapse:separate; border-spacing: 15px;">
+        
+        <tr>
+            <td style="background-color: lightblue;">時間</td>
+            <td style="background-color: lightblue;">星期一</td>
+            <td style="background-color: lightblue;">星期二</td>
+            <td style="background-color: lightblue;">星期三</td>
+            <td style="background-color: lightblue;">星期四</td>
+            <td style="background-color: lightblue;">星期五</td>
+        </tr>
+
+        <tr>
+            <td style="background-color: white;">09:00~12:00</td>
+            <td style="background-color: white;">籃球初階</td>
+            <td style="background-color: white;">羽球</td>
+            <td style="background-color: white;">射箭</td>
+            <td style="background-color: white;">桌球</td>
+            <td style="background-color: white;">初階排球</td>
+        </tr>
+
+        <tr>
+            <td style="background-color: lightpink;">12:00~13:30</td>
+            <td colspan="5" style="background-color: lightpink;">Lunch Time！用餐／午休時間</td>
+        </tr>
+
+          <tr>
+            <td style="background-color: white;">13:30~16:30</td>
+            <td style="background-color: white;">手作羊毛氈杯墊</td>
+            <td style="background-color: white;">黏土畫</td>
+            <td style="background-color: white;">動腦桌遊</td>
+            <td style="background-color: white;">小小富翁理財課</td>
+            <td style="background-color: white;">手作串珠項鍊</td>
+        </tr>
+
+        <tr>
+            <td style="background-color: lightpink;">16:30~17:30</td>
+            <td colspan="5" style="background-color: lightpink;">Tea Time！ 點心時間</td>
+        </tr>
+
+        <tr>
+            <td style="background-color: lightpink;">17:30~</td>
+            <td colspan="5" style="background-color: lightpink;">See you last time！ 快樂回家去 </td>
+        </tr>
+
+        <tr>
+            <td style="background-color: lightpink;">16:30~17:30</td>
+            <td colspan="5" style="background-color: lightpink;">Tea Time！ 點心時間</td>
+        </tr>
+
+    </table>
+
+    <table style="width:100%; text-align:center; border-collapse:separate; border-spacing:10px;">
+    <tr>
+        <td style="background-color: lightblue;">第一梯次<br>7/3~7/7</td>
+        <td style="background-color: lightblue;">第二梯次<br>7/10~7/14</td>
+        <td style="background-color: lightblue;">第三梯次<br>7/17~7/21</td>
+        <td style="background-color: lightblue;">第四梯次<br>7/24~7/28</td>
+        <td style="background-color: lightblue;">第五梯次<br>7/31~8/4</td>
+        <td style="background-color: lightblue;">第六梯次<br>8/7~8/11</td>
+    </tr>
+    <tr>
+        <td colspan="2" style="background-color: lightpink;">早鳥優惠九折起</td>
+        <td colspan="2" style="background-color: lightgreen;">1+1優惠九折</td>
+        <td colspan="2" style="background-color: lightpink;">優惠八折</td>
+    </tr>
+    <tr>
+        <td colspan="2" style="background-color: lightpink;">
+            5/20前報名享九折<br>
+            6/5前報名享九五折
+        </td>
+        <td colspan="2" style="background-color: lightgreen;">
+            即日起~6/5前<br>
+            兩人同時報名同一梯次<br>
+            單人報名兩個梯次
+        </td>
+        <td colspan="2" style="background-color: lightpink;">
+            6/20前<br>
+            一人報名三個梯次
+        </td>
+    </tr>
+</table>
+
+    <p style="text-align: center;background-color: lightblue;">
+        費用:4000元/梯<br>
+        （手作課程材料費700元和射箭課程材料費600元另計）
+    </p>
+
+    <h1 style="text-align:center;"><a href="form.php">手刀報名點此</a></h1>
+    
+</body>
+</HTML>
---
+++ A1133301_王心妤_作業1/form.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>2026一起fun暑假夏令營報名表單</title>
+</head>
+<body>
+    <body style="background-color: lightyellow;">
+    <form action="" method="post">
+    <h1 style="text-align: center;">2026一起fun暑假夏令營報名表單</h1>
+    <p>姓名: <input type="text" placeholder="your name" name="Name" required><br></p>
+    <p>出生年月日: <input type="date" placeholder="your birthday" name="birthday" required><br></p>
+    <p>身分證字號: <input type="id" placeholder="your ID" name="id" required><br></p>
+    <p>
+        年紀: <select name="old">
+        <option value="" selected>小一</option>
+        <option value="">小二</option>
+        <option value="">小三</option>
+        <option value="">小四</option>
+        <option value="">小五</option>
+        <option value="">小六</option>
+        </select>
+    </p>
+    <p>電話: <input type="phone" placeholder="your phone number" name="phone" required><br></p>
+    <p>Email: <input type="email" placeholder="your email" name="email" required><br></p>
+    <p>
+        性別:
+        男<input type="radio" name="mGender" value="m">
+        女<input type="radio" name="mGender" value="f"><br>
+    </p>
+    <p>緊急聯絡人: <input type="text" placeholder="your emergency_name" name="emergency_name" required><br></p>
+    <p>緊急聯絡人電話: <input type="text" placeholder="your emergency_phone" name="emergency_phone" required><br></p>
+    <p>通訊地址: <input type="text" name="address" required><br></p>
+    <p>
+        欲報名梯次:
+        <input type="radio" name="mtired" value="first" required>第一梯次
+        <input type="radio" name="mtired" value="second" required>第二梯次
+        <input type="radio" name="mtired" value="third" required>第三梯次
+        <input type="radio" name="mtired" value="forth" required>第四梯次
+        <input type="radio" name="mtired" value="fifth" required>第五梯次
+        <input type="radio" name="mtired" value="sixth" required>第六梯次
+    <p>
+        繳費方式: <select name="pay">
+        <option value="visa">信用卡</option>
+        <option value="ibon">超商繳費(ibon)</option>
+        <option value="atm">ATM轉帳繳費</option>
+        </select>
+    </p>
+    <p>
+        用餐習慣:
+        <input type="checkbox" name="food" value="葷">葷
+        <input type="checkbox" name="food" value="素">素
+    </p>
+    <p>
+        對於活動的意見:<br>
+        <textarea name="comment" rows="10" cols="50"></textarea>
+    </p>
+    <p style="background-color: lightpink;">
+        活動注意事項!!<br>
+        1.報名後如因故無法參加，請於活動前 14 天通知，可全額退費。<br>
+        2.活動前 7-13 天取消者退費 60%；活動前 3 天內取消恕不退費。<br>
+        3.如遇天災或不可抗力因素導致營隊取消，將統一通知並全額退款。<br>
+        4.主辦單位保有最終修改和變更以及取消本活動之權利。<br>
+    </p>
+        <input type="submit" value="繳交報名表">
+        <input type="reset" value="重填">
+    </p>
+
+    </form>
+</body>
+</html>
